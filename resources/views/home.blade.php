9cf1da6ae244cfc10e3758b1c4542429fc498c76
Jeff Yeon <jeffyeon2061@gmail.com>
2018-05-01 13:40:17 -0700

    add separator

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,6 +68,8 @@
 
                     <h1 class="mb-3">Teams and players</h1>
 
+                    <hr>
+
                     <p v-if="teams.length === 0">No teams yet.</p>
 
                     <div v-for="team in teams" class="mb-3">
